980df116e474432081722ed36c0dce55479da78c
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-01-23 18:40:21 +0300

    Day2's Lesson

---
+++ Day2_Methods/ProductManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_Methods
+{
+    internal class ProductManager
+    { //cw Console WriteLine shortcut
+        public void addProduct(Product product) {
+
+            Console.WriteLine("Added to Inventory " + product.Name);
+            
+        }
+
+
+    }
+}
---
+++ Day2_Methods/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_Methods
+{
+    internal class Program
+    {
+    static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Name = "Apple";
+            product1.Description = "Apple Explanation";
+            product1.Price = 100;
+            product1.Id = 1;
+
+            Product product2 = new Product();
+            product2.Name = "Watermelon";
+            product2.Description = "Watermelon Explanation";
+            product2.Price = 250;
+            product2.Id = 2;
+
+            Product[] products = { product1, product2 };
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.Name);
+                Console.WriteLine(product.Description);
+                Console.WriteLine(product.Price);
+                Console.WriteLine(product.Id);
+                Console.WriteLine("-------------------");
+            }
+            ProductManager productManager = new ProductManager();
+            productManager.addProduct(product1);
+            productManager.addProduct(product2);
+
+
+        }
+
+
+    }
+
+
+}
