4355594aeeed19085b112fbe7cd1d973ee844f0e
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-01-23 17:36:31 +0300

    Add project files.

---
+++ Day1_C#Basics/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_C_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Type Safety
+            string message = "kategory";
+            Console.WriteLine(message);
+            //Do not repeat Yourself
+
+            int studentNumber = 123;
+            double faizRate = 1.45;
+            bool isLogin = false;
+
+            if (isLogin)
+            {
+                Console.WriteLine("Logged");
+            }
+            else
+            {
+                Console.WriteLine("Couldn't Logged");
+            }
+            double dolarYesterday = 10.5;
+            double dolarToday = 11;
+            if (dolarYesterday < dolarToday)
+            {
+                Console.WriteLine("Dolar increased");
+            }
+            else
+            {
+                Console.WriteLine("Dolar decreased");
+            }
+
+
+
+        }
+
+
+    }
+
+}
---
+++ Day1_ClassIntro/ClassIntro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1_ClassIntro
+{
+    internal class ClassIntro
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        class Course {
+        
+        
+        }
+
+
+    }
+
+            
+
+
+
+
+
+}
