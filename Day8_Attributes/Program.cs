6c4e7fe68d5fd0333405f2ad446a064357a8aa23
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-02-17 23:24:29 +0300

    I have learn Attributes and Reflection

---
+++ Day8_Attributes/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8_Attributes
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+             Customer customer = new Customer() { Name = "Bilocan", Id = 258, Age = 33};
+            CustomerDal customerDal = new CustomerDal();
+            customerDal.Add(customer);
+
+
+        }
+
+    }
+
+    [ToTable("Customers")]
+    public class Customer
+    {
+        [RequiredProperty]
+        public string Name { get; set; }
+        [RequiredProperty]
+        public int Id { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class CustomerDal
+    {
+        [Obsolete]
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2}", customer.Name, customer.Id, customer.Age);
+        }
+        
+        public void AddNew(Customer customer)
+        {
+            Console.WriteLine("{0},{1},{2}", customer.Name, customer.Id, customer.Age);
+        }
+
+    }
+    
+    //Attribute targets allow us to where do you use that attribute
+    //AllowMultiple allow us to make usanable twice in same object. Like in toTable attribute we can assign more than one attribute name.
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    class RequiredPropertyAttribute : Attribute 
+    { 
+    
+    }
+
+    class ToTableAttribute : Attribute 
+    {
+        string _NameAttribute;
+        public ToTableAttribute(string name)
+        {
+            _NameAttribute = name;
+        }
+    }
+
+
+}
---
+++ Day8_Reflection/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day8_Reflection
+{       //Reflection create object in running time.
+    public class Program
+    { 
+        static void Main(string[] args)
+        {
+           // Four_Operators four_Operators = new Four_Operators(5, 8);
+           // Console.WriteLine(four_Operators.Add());
+
+            var type = typeof(Four_Operators);
+            Four_Operators four_Operators1 = Activator.CreateInstance(type, 5, 9) as Four_Operators;
+            Console.WriteLine(four_Operators1.Add());
+            Console.WriteLine(four_Operators1.Add(99,22));
+
+        }
+
+    }
+
+    public class Four_Operators
+    {
+        int _number1;
+        int _number2;
+        public Four_Operators(int number1, int number2)
+        {
+            _number1 = number1;
+            _number2 = number2;
+        }
+        public int Add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public int Add()
+        {
+            return _number1 + _number2;
+        }
+
+        public int Subtract(int number1, int number2)
+        { 
+            return number1 - number2; 
+        }
+        
+        public int Multiply(int number1, int number2)
+        {
+            return (number1 * number2);
+        }
+
+        public int Divide(int number1, int number2)
+        {
+            return (number1 / number2);
+        }
+
+
+    }
+
+}
